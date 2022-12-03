f4fe5e5ea06229e12c44dede3aa3506699cc11d0
Irina <irina.sorokina.97@bk.ru>
2022-12-03 18:20:25 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
-﻿ /* Задание 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿ /* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22      */
 
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3       */
 
-
-int a, b;
+int a, b, c;
 Console.WriteLine($"Введите первое число: " );
 int.TryParse (Console.ReadLine ()!, out a); 
 Console.Write ($"Введите второе число: " );
 int.TryParse (Console.ReadLine(), out b);
+Console.Write ($"Введите третье число: " );
+int.TryParse (Console.ReadLine(), out c);
 
- if (a > b)
-{
-   Console.Write($"{a}");
+int max = a;
+if (b > max)
+max = b;
+if (c > max)
+max = c ; 
 
-}
-else
-{
-Console.Write($"{b}");
-}
+Console.Write($"{max}");
